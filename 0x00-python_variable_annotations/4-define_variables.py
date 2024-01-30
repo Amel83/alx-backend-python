e1f898e3873f497c207043628453f1b0ec2a1b51
amel83 <tesfaneshk222@gmail.com>
2024-01-30 17:49:15 +0000

    jkjk

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Vanish me if i don't finish this to annotate
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
